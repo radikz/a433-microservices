2e53504f3b2c41f0806e238315440da00d1fd07c
Rangga Dikarinata <ranggadikarinata@Ranggas-Mac-mini.local>
2023-08-07 08:30:54 +0700

    rename comment build_push_image

--- build_push_image.sh
+++ build_push_image.sh
@@ -3,15 +3,14 @@
 # Build docker image with Dockerfile with name item-app, tag v1
 docker build -t item-app:v1 .
 
-# View list images
+# View the list of Docker images
 docker images
 
-# Naming tag
+# Tag the local image for publishing to GitHub Container Registry
 docker tag item-app:v1 ghcr.io/radikz/a433-microservices/item-app:v1
 
-# Using the CLI for your container type, sign in to the Container registry 
-service at ghcr.io
+# Using the CLI for your container type, sign in to the Container registry service at ghcr.io
 echo $CR_PAT | docker login ghcr.io --username radikz --password-stdin
 
-# Push to Github Packages
+# Push to Github Container Registry
 docker push ghcr.io/radikz/a433-microservices/item-app:v1
